6a86153f984e597084c1044f513f46daafcfb0a9
KHP <61647777+karthik2522@users.noreply.github.com>
2020-09-07 21:48:21 +0530

    Create Vowel.py

---
+++ Vowel.py
@@ -0,0 +1,18 @@
+c=input("Please enter the character as you wish: ")
+
+if(c == 'a' or c == 'A'):
+     print("The given character ",c," is  the vowel")
+
+elif(c == 'e' or c == 'E'):
+     print("The given character ",c," is  the vowel")
+
+elif(c == 'i' or c == 'I'):
+     print("The given character ",c," is  the vowel")
+
+elif(c == 'o' or c == 'O'):
+     print("The given character ",c," is  the vowel")
+
+elif(c == 'u' or c == 'U'):
+     print("The given character ",c," is  the vowel")
+else:
+     print("The given character ",c," is  consonant")
